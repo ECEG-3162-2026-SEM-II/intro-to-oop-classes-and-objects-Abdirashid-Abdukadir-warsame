e08e616e21c9f463f02cc61548cb996a6dba8e8e
Abdirashid-Abdukadir-warsame <enwarkomad@gmail.com>
2026-03-17 11:17:50 -0700

    Added-setter-and-getter
    added getter and setter

--- src/Student.java
+++ src/Student.java
@@ -5,6 +5,25 @@ public class Student {
         this.name = name;
         this.studentID = studentID;
     }
+         //  Getter for name
+    public String getName() {
+        return name;
+    }
+
+    //  Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //  Getter for studentId
+    public int getStudentID() {
+        return studentID;
+    }
+
+    //  Setter for studentId
+    public void setStudentID(int studentId) {
+        this.studentID = studentId;
+    }
 
 
 }
